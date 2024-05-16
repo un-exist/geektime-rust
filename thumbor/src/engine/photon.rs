636c47b1fa68ea8e3dc4b6c7bc93477c4c11f96d
zhangdecheng <347612433@qq.com>
2024-05-16 22:50:05 +0800

    commit

---
+++ thumbor/src/engine/photon.rs
@@ -0,0 +1,133 @@
+use super::{Engine, SpecTransform};
+use crate::pb::*;
+use anyhow::Result;
+use bytes::Bytes;
+use image::{DynamicImage, ImageBuffer, ImageFormat};
+use lazy_static::lazy_static;
+use photon_rs::{
+    effects, filters, multiple, native::open_image_from_bytes, transform, PhotonImage,
+};
+use std::{convert::TryFrom, io};
+
+lazy_static! {
+    // 预先把水印文件加载为静态变量
+    static ref WATERMARK: PhotonImage = {
+
+        // 这里你需要把我 github 项目下的对应图片拷贝到你的根目录
+        // 在编译的时候 include_bytes! 宏会直接把文件读入编译后的二进制
+        let data = include_bytes!("../../rust-logo.png");
+        let watermark = open_image_from_bytes(data).unwrap();
+        transform::resize(&watermark, 64, 64, transform::SamplingFilter::Nearest)
+    };
+}
+
+// 我们目前支持 Photon engine
+pub struct Photon(PhotonImage);
+
+// 从 Bytes 转换成 Photon 结构
+impl TryFrom<Bytes> for Photon {
+    type Error = anyhow::Error;
+
+    fn try_from(data: Bytes) -> Result<Self, Self::Error> {
+        Ok(Self(open_image_from_bytes(&data)?))
+    }
+}
+
+impl Engine for Photon {
+    fn apply(&mut self, specs: &[Spec]) {
+        for spec in specs.iter() {
+            match spec.data {
+                Some(spec::Data::Crop(ref v)) => self.transform(v),
+                Some(spec::Data::Contrast(ref v)) => self.transform(v),
+                Some(spec::Data::Filter(ref v)) => self.transform(v),
+                Some(spec::Data::Fliph(ref v)) => self.transform(v),
+                Some(spec::Data::Flipv(ref v)) => self.transform(v),
+                Some(spec::Data::Resize(ref v)) => self.transform(v),
+                Some(spec::Data::Watermark(ref v)) => self.transform(v),
+                // 对于目前不认识的 spec，不做任何处理
+                _ => {}
+            }
+        }
+    }
+
+    fn generate(self, format: ImageFormat) -> Vec<u8> {
+        image_to_buf(self.0, format)
+    }
+}
+
+impl SpecTransform<&Crop> for Photon {
+    fn transform(&mut self, op: &Crop) {
+        let img = transform::crop(&mut self.0, op.x1, op.y1, op.x2, op.y2);
+        self.0 = img;
+    }
+}
+
+impl SpecTransform<&Contrast> for Photon {
+    fn transform(&mut self, op: &Contrast) {
+        effects::adjust_contrast(&mut self.0, op.contrast);
+    }
+}
+
+impl SpecTransform<&Flipv> for Photon {
+    fn transform(&mut self, _op: &Flipv) {
+        transform::flipv(&mut self.0)
+    }
+}
+
+impl SpecTransform<&Fliph> for Photon {
+    fn transform(&mut self, _op: &Fliph) {
+        transform::fliph(&mut self.0)
+    }
+}
+
+impl SpecTransform<&Filter> for Photon {
+    fn transform(&mut self, op: &Filter) {
+        match i32::try_into(op.filter) {
+            Ok(filter::Filter::Unspecified) => {}
+            Ok(f) => filters::filter(&mut self.0, f.to_str().unwrap()),
+            Err(_) => {}
+        }
+    }
+}
+
+impl SpecTransform<&Resize> for Photon {
+    fn transform(&mut self, op: &Resize) {
+        let img = match i32::try_into(op.rtype).unwrap() {
+            resize::ResizeType::Normal => transform::resize(
+                &mut self.0,
+                op.width,
+                op.height,
+                resize::SampleFilter::try_from(op.filter).unwrap().into(),
+            ),
+            resize::ResizeType::SeamCarve => {
+                transform::seam_carve(&mut self.0, op.width, op.height)
+            }
+        };
+        self.0 = img;
+    }
+}
+
+impl SpecTransform<&Watermark> for Photon {
+    fn transform(&mut self, op: &Watermark) {
+        multiple::watermark(&mut self.0, &WATERMARK, op.x, op.y);
+    }
+}
+
+// photon 库竟然没有提供在内存中对图片转换格式的方法，只好手工实现
+fn image_to_buf(img: PhotonImage, format: ImageFormat) -> Vec<u8> {
+    let raw_pixels: Vec<u8> = img.get_raw_pixels();
+    let width: u32 = img.get_width();
+    let height: u32 = img.get_height();
+
+    let img_buffer: ImageBuffer<image::Rgba<u8>, Vec<u8>> = ImageBuffer::from_vec(width, height, raw_pixels).unwrap();
+    let dynimage = DynamicImage::ImageRgba8(img_buffer).to_rgb8();
+    // 将图像转换为 RGB 格式
+
+    // 创建一个 Cursor<Vec<u8>> 作为写入器
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut cursor = io::Cursor::new(&mut buffer);
+
+    // 现在写入图像数据到 cursor
+    dynimage.write_to(&mut cursor, format).unwrap();
+    buffer
+}
---
+++ thumbor/src/main.rs
@@ -0,0 +1,129 @@
+use anyhow::Error;
+use axum::{
+    extract::{Extension, Path},
+    http::{HeaderMap, HeaderValue, StatusCode},
+    routing::get,
+    Router,
+};
+use bytes::Bytes;
+use lru::LruCache;
+use pb::ImageSpec;
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
+use serde::Deserialize;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    num::NonZeroUsize,
+    sync::Arc,
+};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tower_http::add_extension::AddExtensionLayer;
+use tracing::{info, instrument};
+
+use engine::{Engine, Photon};
+use pb::*;
+
+mod engine;
+mod pb;
+
+// 参数使用 serde 做 Deserialize，axum 会自动识别并解析
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    // 初始化 tracing
+    tracing_subscriber::fmt::init();
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(NonZeroUsize::new(1024).unwrap())));
+
+    // 构建路由
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = "127.0.0.1:3000";
+
+    print_test_url("https://images.pexels.com/photos/1562477/pexels-photo-1562477.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+// 目前我们就只把参数解析出来
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    let url = &percent_decode_str(&url).decode_utf8_lossy();
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    // 使用 image engine 处理
+    let mut engine: Photon = data
+        .try_into()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    engine.apply(&spec.specs);
+
+    let image = engine.generate(image::ImageFormat::Jpeg);
+
+    info!("Finished processing: image size {}", image.len());
+    let mut headers = HeaderMap::new();
+
+    headers.insert("content-type", HeaderValue::from_static("image/jpeg"));
+    Ok((headers, image))
+}
+
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes, Error> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+    let g = &mut cache.lock().await;
+
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+    Ok(data)
+}
+
+// 调试辅助函数
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{}/{}", s, test_image);
+}
